c3c09014ab9ea44cb45da6e3a0df626e5f7f8acb
Lieven Maus <lievenmaus@outlook.com>
2026-07-30 16:36:46 +0200

    2026-07-30 16:36:50

---
+++ magweg.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ *
+ * @file        magweg.php
+ * @author      lm
+ * @dateCreated Thu 2026-07-30 16:36:34
+ * @dateLastMod Thu 2026-07-30 16:36:38
+ *
+ * @copyright   Copyright 1981-present - Lieven Maus <info@grompil.com>
+ *
+ * @wiki
+ *
+**/
+
+if (!defined('BASIC_INDEX_SEEN')) {	require $_SERVER['DOCUMENT_ROOT'] . '/not_allowed.php'; }
