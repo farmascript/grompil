c3aa0c4e3e9d3f7f2ce2a65f318c216209252026
Lieven Maus <lievenmaus@outlook.com>
2026-07-31 21:08:17 +0200

    2026-07-31 21:08:23

---
+++ media/index.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ *
+ * @file        index.php
+ * @author      lm
+ * @dateCreated Fri 2026-07-31 21:08:04
+ * @dateLastMod Fri 2026-07-31 21:08:06
+ *
+ * @copyright   Copyright 1981-present - Lieven Maus <info@grompil.com>
+ *
+ * @wiki
+ *
+**/
+
+if (!defined('BASIC_INDEX_SEEN')) {	require $_SERVER['DOCUMENT_ROOT'] . '/not_allowed.php'; }
