2775a6895b9a4693070d5f94f22ad8567c045219
Lieven Maus <lievenmaus@outlook.com>
2026-07-31 21:20:21 +0200

    2026-07-31 21:20:24 added index.php files to all folders in src, config and tmpl. This is to prevent directory listing on web servers that have this enabled.

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ *
+ * @file        index.php
+ * @author      lm
+ * @dateCreated Fri 2026-07-31 21:12:46
+ * @dateLastMod Fri 2026-07-31 21:12:48
+ *
+ * @copyright   Copyright 1981-present - Lieven Maus <info@grompil.com>
+ *
+ * @wiki
+ *
+**/
+
+if (!defined('BASIC_INDEX_SEEN')) {	require $_SERVER['DOCUMENT_ROOT'] . '/not_allowed.php'; }
---
+++ src/lang/index.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ *
+ * @file        index.php
+ * @author      lm
+ * @dateCreated Fri 2026-07-31 21:13:46
+ * @dateLastMod Fri 2026-07-31 21:13:49
+ *
+ * @copyright   Copyright 1981-present - Lieven Maus <info@grompil.com>
+ *
+ * @wiki
+ *
+**/
+
+if (!defined('BASIC_INDEX_SEEN')) {	require $_SERVER['DOCUMENT_ROOT'] . '/not_allowed.php'; }
